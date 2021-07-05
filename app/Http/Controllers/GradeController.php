40e66b6727e95520ee36caa9a3aec6d267384565
Tom-vdm-Hz <muij0013@hz.nl>
2021-07-05 16:56:34 +0200

    Create GradeController.php

---
+++ app/Http/Controllers/GradeController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Grade;
+use Illuminate\Http\Request;
+
+class GradeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('grades.index', ['grades' =>  Grade::all()]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('grades.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+    Grade::create($this->validateGrade());
+    return redirect(route('grades.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Grade  $grade
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return view('grades.show', ['grade' =>  Grade::find($id)]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Grade  $grade
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return view('grades.edit', ['grade' =>  Grade::find($id)]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Grade  $grade
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        Grade::find($id)->update($this->validateGrade());
+        return redirect(route('grades.index'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Grade  $grade
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Grade::destroy($id);
+        return redirect(route('grades.index'));
+    }
+
+    /**
+     * @return array
+     */
+    public function validateGrade(): array
+    {
+        return request()->validate([
+            'name' => ['required', 'string'],
+            'grade' => ['required', 'integer'],
+        ]);
+    }
+}
