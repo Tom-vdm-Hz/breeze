fc718e9b643332c4daeffa0deb77710ca73bb477
Tom-vdm-Hz <muij0013@hz.nl>
2021-07-06 13:39:14 +0200

    Update create.blade.php

--- resources/views/grades/create.blade.php
+++ resources/views/grades/create.blade.php
@@ -4,7 +4,8 @@
             Make Grade
         </h2>
     </x-slot>
-
+    <link rel="stylesheet" href="../../css/app.css">
+    <link rel="stylesheet" href="../../../public/css/app.css">
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
