096087f3b1b88c2cc3d6257302ba8ba17ffbd8dc
Tom-vdm-Hz <muij0013@hz.nl>
2021-07-06 13:35:15 +0200

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,7 +11,7 @@
 
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
+        <link rel="stylesheet" href="resources/css/app.css">
         <form method="POST" action="{{ route('login') }}">
             @csrf
 
