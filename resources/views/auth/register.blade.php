237777313f6865f7c6fb32f98e86c4889b104264
Tom-vdm-Hz <muij0013@hz.nl>
2021-07-06 13:39:21 +0200

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,8 @@
                 <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
             </a>
         </x-slot>
-
+        <link rel="stylesheet" href="../../css/app.css">
+        <link rel="stylesheet" href="../../../public/css/app.css">
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
