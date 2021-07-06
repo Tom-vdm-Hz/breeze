7ded4cfc43e5fdff6c32db37c3b8b71ed235946f
Tom-vdm-Hz <muij0013@hz.nl>
2021-07-06 13:39:02 +0200

    Update navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -10,7 +10,8 @@
                         <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
                     </a>
                 </div>
-
+                <link rel="stylesheet" href="../../css/app.css">
+                <link rel="stylesheet" href="../../../public/css/app.css">
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
