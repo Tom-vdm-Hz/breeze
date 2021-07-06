82def15c9d19aabae971adc3878026044df86bce
Tom-vdm-Hz <muij0013@hz.nl>
2021-07-06 13:39:05 +0200

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,7 +6,8 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
-
+        <link rel="stylesheet" href="../../css/app.css">
+        <link rel="stylesheet" href="../../../public/css/app.css">
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
