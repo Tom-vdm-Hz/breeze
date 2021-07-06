724dbe1b9602b9919e5c61e727361b81f58c47fd
Tom-vdm-Hz <muij0013@hz.nl>
2021-07-06 13:39:24 +0200

    Update confirm-password.blade.php

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -5,7 +5,8 @@
                 <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
             </a>
         </x-slot>
-
+        <link rel="stylesheet" href="../../css/app.css">
+        <link rel="stylesheet" href="../../../public/css/app.css">
         <div class="mb-4 text-sm text-gray-600">
             {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
         </div>
